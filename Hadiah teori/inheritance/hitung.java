f5c677e5e81d39c7b3dcd76e66eae1f2723f9c83
Jervin descrates kontessa <71800787+Jervinkontessa@users.noreply.github.com>
2020-11-15 19:03:24 +0800

    Update hitung.java

--- Hadiah teori/inheritance/hitung.java
+++ Hadiah teori/inheritance/hitung.java
@@ -2,6 +2,6 @@ class hitung extends volume{
 	int jari2;
 	
 	void hitung(){
-		System.out.println("volume lingkaran = "+ (4/3*PI*jari2*jari2*jari2));
+		System.out.println("volume bola adalah = "+ (4/3*PI*jari2*jari2*jari2));
 	}
 }
